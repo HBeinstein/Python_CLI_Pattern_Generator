fe6f04e3ebed5dbb70350e4562d633a1e72cfaf8
Hannah Beinstein <hannah.beinstein@gmail.com>
2020-08-10 21:06:39 -0700

    add algorithm to calc average pixel darkness and replace with corresponding symbol

--- program.py
+++ program.py
@@ -1,4 +1,26 @@
-import requests
+import requests 
+import sys
+from PIL import Image 
+from io import BytesIO
 
-response = requests.get("https://picsum.photos/200/300")
-print(response.url)
+response_img = requests.get("https://picsum.photos/120/20")
+print(response_img.url)
+
+img = Image.open(BytesIO(response_img.content))
+print(img)
+
+width, height = img.size
+print(width, height)
+
+img = img.convert("L")
+
+pixels = img.getdata()
+
+symbols = ["B","S","@","&","$","%","*","!",":","^","."]
+display_pixels = [symbols[pixel // 25] for pixel in pixels]
+display_pixels = ''.join(display_pixels)
+
+display_pixels_count = len(display_pixels)
+result = [display_pixels[index:index + width] for index in range(0, display_pixels_count, width)]
+result = "\n".join(result)
+print(result)
